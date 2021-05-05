8401aeff9a51584d06f858ea37d54b4365d65602
Adedeji Onigbinde <adedejionigbinde@gmail.com>
2021-05-05 22:49:19 +0100

    Create Practice6.vb
    Practice 6

---
+++ Practice6.vb
@@ -0,0 +1,14 @@
+Imports System
+'18/19 Section C Number 3b
+Module Module1
+    Sub Main()
+        Dim intBonus As Integer 
+        Dim decSales As Integer 
+        If (decSales >= 50000) Then
+            intBonus = 2000
+        Else
+            intBonus = 500
+        End If
+    End Sub
+End Module
+        
