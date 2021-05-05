ba8126195d25d11b1738e14faa9e83719ac3e8b6
Adedeji Onigbinde <adedejionigbinde@gmail.com>
2021-05-05 22:16:43 +0100

    Create Practice4.vb
    Practice 4

---
+++ Practice4.vb
@@ -0,0 +1,27 @@
+Imports System
+'18/19 Section C Question 2bi and 2bii
+Module Addition
+    Sub Main()
+
+        '2bi
+        Dim count As Integer = 0
+        Dim sum As Integer = 0
+        Do
+            If (count % 2 = 0) Then 
+                sum += count
+            End If
+            count ++
+        Loop While (count <= 100)
+        Console.WriteLine("The Sum Is: " & sum)
+        
+        '2bii
+         Do
+            If (count % 2 = 0) Then 
+                sum += count
+            End If
+            count ++
+        Loop Until count <= 100
+        Console.WriteLine("The Sum Is: " & sum)
+
+    End Sub
+End Module
