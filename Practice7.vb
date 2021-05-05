c3909aaac1700618d6cc1df693859d019e884106
Adedeji Onigbinde <adedejionigbinde@gmail.com>
2021-05-05 23:10:30 +0100

    Update Practice7.vb
    Practice 7

--- Practice7.vb
+++ Practice7.vb
@@ -1,6 +1,7 @@
 Imports System
-'18/19 Section C Question 3c
+'18/19 Section C Question 3ci and 3cii
 Module MultiplesOfThree
+    '3ci
     Sub Main()
         Dim count As Integer 
         Dim sum As Integer = 0
@@ -8,5 +9,15 @@ Module MultiplesOfThree
             sum+=count
         Next 
             Console.WriteLine("The Sum Is: " & sum)
+
+    '3cii
+        Do 
+            If (count % 3 = 0) Then
+                sum+=count
+            End If
+            count++
+        Loop While (count <= 100)
+        Console.WriteLine("The Sum Is: " & sum)
+            
     End Sub
 End Module
